5320b7833bf11681f07358feef3543568bc2e873
nursultank <nursultankazbekov23@gmail.com>
2022-05-28 10:11:22 +0300

    update list component

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -8,7 +8,7 @@ const List = ({items}) => {
     return <ul className="list">
         {
             items.map(item => (
-                <li className="active">
+                <li className={item.active ? 'active' : ''}>
                     <i>{item.icon ? item.icon : <i className={`badge badge--${item.color}`}></i>}</i>
                     <span>{item.name}</span>
                 </li>
