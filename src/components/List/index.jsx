de9481133a6d633d3b0573af213b6a96d91072e0
Nursultan <nursultankazbekov23@gmail.com>
2022-06-29 09:33:23 +0600

    added active item default

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -22,7 +22,7 @@ const List = ({items, isRemovable, onClick, onRemove, onClickItem, activeItem}) 
             items.map((item, index) => (
                 <li
                     key={index}
-                    className={classNames(item.className, {'active': activeItem && activeItem.id === item.id})}
+                    className={classNames(item.className, {'active': item.active ? item.active : activeItem && activeItem.id === item.id})}
                     onClick={onClickItem ? () => onClickItem(item) : null}
                 >
                     <i>
